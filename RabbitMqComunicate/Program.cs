553e090d8651f27a652bd980dfd65fab481cd913
Ramon Xavier <ramon.nascimento@guiandotelecom.com.br>
2023-10-10 22:19:21 -0300

    Add project files.

---
+++ RabbitMqComunicate/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Configurações de conexão com o RabbitMQ
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost", // Use "localhost" para o host
+    Port = 8080,
+    UserName = "user",
+    Password = "password",
+    ClientProvidedName = "teste",
+};
+
+// Criar uma conexão com o RabbitMQ
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+const string queueName = "fila_teste";
+
+channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Mensagem recebida: {message}");
+};
+
+// Registre o consumidor na fila
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
